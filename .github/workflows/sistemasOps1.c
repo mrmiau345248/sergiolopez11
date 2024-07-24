a1a4e442044812a778572ab17358fa3abd089601
mrmiau345248 <147886893+mrmiau345248@users.noreply.github.com>
2024-07-24 02:33:46 -0500

    Create sistemasOps1.c
    Presentacion de la clase de Sistemas Operativos 2024

---
+++ .github/workflows/sistemasOps1.c
@@ -0,0 +1,133 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+struct persona {
+    char nombre[50];
+    char apellido[60];
+    char carrera[50];
+    char semestre[50];
+    char exp[1500];
+};
+
+struct materia {
+    char nombre[60];
+    int creditos;
+    char salon[60];
+};
+struct materia materias[] = {
+    {"Estructuras de Datos", 3, "Ingenieria: 106"},
+    {"Sistemas de Informacion", 3, "Baron: 608"},
+    {"Sistemas Operativos", 3, "Baron: 002"},
+    {"Hoja de calculo", 2, "Baron: s101"},
+    {"Fundamentos de Software", 3, "Ingenieria: 96C"},
+    {"Etica", 2, "Baron: 403"},
+    {"Probabilidad", 3, "Baron: 503"}
+};
+void asignarAMemoria(int *p, int v){
+  *p=v;
+}
+struct materia* buscarMateria(char *nombre) {
+    for(int i=0; i <7; i++) {
+        if (strcmp(materias[i].nombre, nombre) == 0) {
+            return &materias[i];
+        }
+    }
+    return NULL;
+}
+
+struct persona p1 = {"Sergio","Lopez",  "Ingenieria de Sistemas", "Tercer Semestre",
+    "\n\tTengo experiencia en back end y front end de desarrollo de proyecto de RenCar, aplicacion usada para la gestion "
+    "\tde automoviles y servicio de parqueadero. Desarrolle dos proyecto de redes y telocomucaciones de manera\n "
+    "\tindividual siendo el primero el modelaje y conexion a NAT\n"
+    "\tde dos edificios con 4 departamentos los cuales adicionalmente contaron con servidor DNS, redundancia en\n"
+    "\tlas conexiones y protocolo de Spanning Three en las mismas. Todo esto dividiendo la red en varias subredes\n"
+    "\ty realizando las diferentes asignaciones a cada uno de los departamentos.\n"
+    "\tTodo esto haciendo uso de la herramineta GNS3 y WireShark. El segundo proyecto desarrollado fue\n"
+    "\tel diseño y programacion de un servidor DNS en Java, completamente funcional para ser\n"
+    "\timplementado dentro de la topologia previamente disañada y probada en GNS3.\n "
+    "\tActualmente me encuentro cursando certificacion de ISC2 en seguridad de la informacion asi como\n "
+    "\tdesenvolviendome en desarrollo WEB en HTML, CSS y Java Script.\n"};
+
+
+int main(int argc, char *argv[]) {
+    int valor;
+    int* opcion=&valor;
+    printf("Miauuuuuu %d\n ", *opcion);
+
+
+    printf("Ingrese la opcion que desea escoger\n"
+           "\t1. Para nombre y carrera.\n"
+           "\t2.Para nombre y experiencia.\n"
+           "\t3.Para nombre.\n"
+           "\t4.Para buscar materia.\n"
+           "\t5.Para nombre y carrera de buses.\n");
+    scanf("%d",opcion);
+    switch (*opcion){
+        case 1:
+        const char *presentacion = "\t\t!!!!!!!!!!!!!!!PRESENTACION!!!!!!!!!!!!!!!\n"
+                                   "\tNOMBRE: \t%s\n"
+                                   "\tAPELLIDO:\t%s\n"
+                                   "\tCARRERA: \t%s\n"
+                                   "\tSEMESTRE: \t%s\n"
+                                   "\t<---------------------------MATERIAS--------------------------->\n";
+        printf("+");
+        for (int i = 0; i < 80; i++) {
+            printf("-");
+        }
+        printf("+\n");
+
+        printf("| ");
+        printf(presentacion, p1.nombre,p1.apellido, p1.carrera, p1.semestre);
+
+        for (int i = 0; i < 7; i++) {
+            printf("\t%s, \t%d creditos, en \t%s\n", materias[i].nombre, materias[i].creditos, materias[i].salon);
+        }
+
+        printf(" |\n");
+        printf("+");
+        for (int i = 0; i < 80; i++) {
+            printf("-");
+        }
+        printf("+\n");
+
+
+            break;
+        case 2:
+            presentacion = "\t\t!!!!!!!!!!!!!!!PRESENTACION!!!!!!!!!!!!!!!\n"
+                                  "\tNOMBRE: \t%s\n"
+                                  "\tAPELLIDO:\t%s\n"
+                                  "%s";
+            printf(presentacion,p1.nombre,p1.apellido, p1.exp);
+
+
+            break;
+        case 3:
+
+
+            presentacion = "\t\t!!!!!!!!!!!!!!!PRESENTACION!!!!!!!!!!!!!!!\n"
+                                             "\tNOMBRE: \t%s\n"
+                                             "\tAPELLIDO:\t%s\n";
+        printf(presentacion,p1.nombre,p1.apellido);
+        break;
+        case 4:
+             char valor[50];
+             char* miau=valor;
+        printf("Ingrese la materia a encontrar: -> ");
+            scanf("%s",miau);
+        if(buscarMateria(miau)!=NULL) {
+            printf("\tLa materia solicitada es:\n"
+                   "\tNOMBRE: %s\n"
+                   "\tCREDITOS: %d\n"
+                   "\tSALON: %s\n",buscarMateria(miau)->nombre,buscarMateria(miau)->creditos,buscarMateria(miau)->salon);
+        }else {
+            printf("Materia no encontrada\n");
+        }
+            break;
+
+        default:
+
+            printf("Que putas esta metiendo cacorro hijo de perra");
+    };
+    return 0;
+}

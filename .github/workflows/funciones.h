fcc48d862e18601f0c4975a58580b46e4c8d6d78
mrmiau345248 <147886893+mrmiau345248@users.noreply.github.com>
2024-07-25 17:26:29 -0500

    funciones.h

---
+++ .github/workflows/funciones.h
@@ -0,0 +1,11 @@
+//
+// Created by sl397 on 25/07/2024.
+//
+
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+float calcularCirculo(float r);
+float calcularVolumenCirculo(float r);
+float calcularAreaCono(float r, float h);
+float calcularHipo(float c,float c1);
+#endif //FUNCIONES_H

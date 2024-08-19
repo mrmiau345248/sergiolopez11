c26f0ec4537f0383b90deedc1fe3bb3650b191b4
mrmiau345248 <147886893+mrmiau345248@users.noreply.github.com>
2024-08-19 11:57:14 -0500

    Update main.c

--- .github/SistemasOperativos/Clase3-5/ManejoDeMatrices/main.c
+++ .github/SistemasOperativos/Clase3-5/ManejoDeMatrices/main.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include "funciones.h"
 
-
-
 int main(int argc, char *argv[]) {
     if (argc != 2) { // Debe haber un argumento para el tamaño de la matriz
         fprintf(stderr, "Uso: %s <Tamaño>\n", argv[0]);
@@ -18,27 +16,47 @@ int main(int argc, char *argv[]) {
     }
 
     // Crear y llenar las matrices
-    for (int k = 1; k <= 3; k++) {
-        // Asignar memoria para la matriz
-        int **matriz = (int **)malloc(tamaño * sizeof(int *));
-        for (int i = 0; i < tamaño; i++) {
-            matriz[i] = (int *)malloc(tamaño * sizeof(int));
-        }
+    int **matriz1 = (int **)malloc(tamaño * sizeof(int *));
+    int **matriz2 = (int **)malloc(tamaño * sizeof(int *));
+    int **resultado = (int **)malloc(tamaño * sizeof(int *));
 
-        // Llenar la matriz con el mismo valor (k)
-        for (int i = 0; i < tamaño; i++) {
-            for (int j = 0; j < tamaño; j++) {
-                matriz[i][j] = k; // Llena la matriz con el valor k
-            }
-        }
-
-        // Imprimir la matriz
-        printf("Matriz %d x %d:\n", tamaño, tamaño);
-        imprimirMatriz(matriz, tamaño);
-
-        // Liberar memoria
-        liberarMatriz(matriz, tamaño);
+    for (int i = 0; i < tamaño; i++) {
+        matriz1[i] = (int *)malloc(tamaño * sizeof(int));
+        matriz2[i] = (int *)malloc(tamaño * sizeof(int));
+        resultado[i] = (int *)malloc(tamaño * sizeof(int));
     }
+
+    // Llenar la primera matriz con el valor 1
+    for (int i = 0; i < tamaño; i++) {
+        for (int j = 0; j < tamaño; j++) {
+            matriz1[i][j] = 1;
+        }
+    }
+
+    // Llenar la segunda matriz con el valor 2
+    for (int i = 0; i < tamaño; i++) {
+        for (int j = 0; j < tamaño; j++) {
+            matriz2[i][j] = 2;
+        }
+    }
+
+    // Multiplicar las matrices
+    multiplicarMatrices(matriz1, matriz2, resultado, tamaño);
+
+    // Imprimir las matrices
+    printf("Matriz 1 (%d x %d):\n", tamaño, tamaño);
+    imprimirMatriz(matriz1, tamaño);
+
+    printf("Matriz 2 (%d x %d):\n", tamaño, tamaño);
+    imprimirMatriz(matriz2, tamaño);
+
+    printf("Resultado de la multiplicación (%d x %d):\n", tamaño, tamaño);
+    imprimirMatriz(resultado, tamaño);
+
+    // Liberar memoria
+    liberarMatriz(matriz1, tamaño);
+    liberarMatriz(matriz2, tamaño);
+    liberarMatriz(resultado, tamaño);
 
     return 0;
 }

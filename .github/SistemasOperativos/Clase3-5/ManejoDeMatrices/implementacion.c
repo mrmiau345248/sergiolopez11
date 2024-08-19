c4fae9e6324458617a9d4994ccfe9a7739cfc0a6
mrmiau345248 <147886893+mrmiau345248@users.noreply.github.com>
2024-08-19 11:57:29 -0500

    Update implementacion.c

--- .github/SistemasOperativos/Clase3-5/ManejoDeMatrices/implementacion.c
+++ .github/SistemasOperativos/Clase3-5/ManejoDeMatrices/implementacion.c
@@ -1,9 +1,7 @@
-//
-// Created by sl397 on 19/08/2024.
-//
 #include "funciones.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 
 void imprimirMatriz(int **matriz, int tamaño) {
     for (int i = 0; i < tamaño; i++) {
@@ -20,4 +18,15 @@ void liberarMatriz(int **matriz, int tamaño) {
         free(matriz[i]);
     }
     free(matriz);
+}
+
+void multiplicarMatrices(int **matriz1, int **matriz2, int **resultado, int tamaño) {
+    for (int i = 0; i < tamaño; i++) {
+        for (int j = 0; j < tamaño; j++) {
+            resultado[i][j] = 0;
+            for (int k = 0; k < tamaño; k++) {
+                resultado[i][j] += matriz1[i][k] * matriz2[k][j];
+            }
+        }
+    }
 }

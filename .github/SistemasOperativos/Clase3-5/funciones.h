f16c6b24684f1559383fde75d8158b39cb5d09d8
mrmiau345248 <147886893+mrmiau345248@users.noreply.github.com>
2024-08-19 11:34:46 -0500

    Create funciones.h

---
+++ .github/SistemasOperativos/Clase3-5/funciones.h
@@ -0,0 +1,24 @@
+//
+// Created by sl397 on 1/08/2024.
+//
+
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+void suma();
+void direccion();
+void funcion3();
+void funcion4();
+void funcion5();
+char* function( char* p);
+void funcion6();
+void funcion7();
+void funcion8();
+void funcion9(int argc, char *argv[]);
+void funcion10();
+void funcion11();
+void funcion12();
+void funcion13();
+void funcion14();
+
+
+#endif //FUNCIONES_H

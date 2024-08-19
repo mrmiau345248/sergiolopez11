c47f9add10386919a96eb4396044beddc0fd8655
mrmiau345248 <147886893+mrmiau345248@users.noreply.github.com>
2024-08-19 11:51:31 -0500

    Create funciones.h

---
+++ .github/SistemasOperativos/Clase3-5/ManejoDeMatrices/funciones.h
@@ -0,0 +1,11 @@
+//
+// Created by sl397 on 19/08/2024.
+//
+
+#ifndef MATRICES_FUNCIONES_H
+#define MATRICES_FUNCIONES_H
+#include <stdio.h>
+#include <stdlib.h>
+void imprimirMatriz(int **matriz, int tamaño) ;
+void liberarMatriz(int **matriz, int tamaño) ;
+#endif //MATRICES_FUNCIONES_H

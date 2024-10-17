aa8f61680ed3c5ea0edbbd919bf5b34cdaba405d
mrmiau345248 <147886893+mrmiau345248@users.noreply.github.com>
2024-10-17 16:31:05 -0500

    Create main.cpp

---
+++ 4to_Semestre_2403/Estructuras/Entregas_Proyecto/Entrega1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "Vertice.h"
+#include "Figura.h"
+#include "sistema.h"
+#include "Mesh.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+   Sistema s;
+   cout << "Bienvenido" << endl;
+    string comando;
+    s.menu(comando);
+}
